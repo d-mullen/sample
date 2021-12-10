244ba4221053d41601706af3108ad7703246055f
Dan Mullen <danmullens@aus-1927-ml.attlocal.net>
2021-12-10 09:40:11 -0600

    changes

--- testperf.go
+++ testperf.go
@@ -12,16 +12,17 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
+	"google.golang.org/api/iterator"
 )
 
-func resetFB(ctx context.Context, project string) error {
+func ResetEmulator(ctx context.Context, project string) error {
 	fsHost := os.Getenv("FIRESTORE_EMULATOR_HOST")
 	fmt.Printf("Reset emulator:%s\n", fsHost)
 	if fsHost == "" {
 		return errors.New("FIRESTORE_EMULATOR_HOST is not set")
 	}
 	client := &http.Client{}
-	r, err := http.NewRequestWithContext(ctx, http.MethodDelete, fmt.Sprintf("http://%s/emulator/v1/projects/%s/databases/(default)/documents", fsHost, project), nil)
+	r, err := http.NewRequestWithContext(ctx, "DELETE", fmt.Sprintf("http://%s/emulator/v1/projects/%s/databases/(default)/documents", fsHost, project), nil)
 	if err != nil {
 		return err
 	}
@@ -30,8 +31,8 @@ func resetFB(ctx context.Context, project string) error {
 }
 
 func createClient(ctx context.Context) *firestore.Client {
-	projectID := "dev"
-	err := resetFB(ctx, projectID)
+	projectID := "zing-dev-adjunct"
+	//	err := ResetEmulator(ctx, projectID)
 	client, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
@@ -39,23 +40,12 @@ func createClient(ctx context.Context) *firestore.Client {
 	return client
 }
 
-func main() {
-	var DocCount = 10000
-	var ColName = "perftesting"
-
-	// Get a Firestore client.
-	ctx := context.Background()
-	client := createClient(ctx)
-	defer client.Close()
-	fmt.Println("Created client")
-	// [START firestore_setup_dataset_pt1]
-	col := client.Collection(ColName)
-	fmt.Println("Created collection")
+func addEventsToCollection(ctx context.Context, DocCount int, col *firestore.CollectionRef) (err error) {
 	baseTime := time.Now().Add(-7 * time.Hour * 24)
 	for i := 0; i < DocCount; i++ {
 		uuid_1, _ := uuid.New()
 		uuid_2, _ := uuid.New()
-		_, _, err := col.Add(ctx, map[string]interface{}{
+		_, _, err = col.Add(ctx, map[string]interface{}{
 			"_id":       fmt.Sprintf("%X", uuid_1),
 			"EventID":   fmt.Sprintf("%X", uuid_2),
 			"TenantID":  rand.Intn(5),
@@ -68,25 +58,66 @@ func main() {
 		baseTime = baseTime.Add(10 * time.Second)
 	}
 	fmt.Printf("Inserted %d documents\n", DocCount)
+	return err
+}
+
+func main() {
+	//var DocCount = 10000
+	var tenant = "smoke-tenant"
+	var ColName = fmt.Sprintf("EventContextTenants/%s/Events", tenant)
+
+	// Get a Firestore client.
+	ctx := context.Background()
+	client := createClient(ctx)
+	defer client.Close()
+	log.Println("Created client")
+
+	/*
+		collections, err := client.Collections(ctx).GetAll()
+		if err != nil {
+			log.Printf("%v\n", err)
+			log.Fatal("Something wrong with the client")
+		}
+
+		for _, c := range collections {
+			log.Printf("collection: %s", c.Path)
+		}
+	*/
+
+	col := client.Collection(ColName)
+
+	if col == nil {
+		log.Fatalf("Could not get collection [%s]", ColName)
+	}
+
+	log.Printf("Found Collection %s", col.Path)
 
 	// Query the collection.
 	start := time.Now()
-	q := col.Select("EventID").Where("TenantID", "==", 1).
-		OrderBy("timestamp", firestore.Desc).Limit(2000)
-	docIter, err := q.Documents(ctx).GetAll()
+	q := col.Select("id").OrderBy("createdAt", firestore.Desc)
 
-	if err != nil {
-		log.Fatalf("Failed to get docs, %v", err)
-	}
+	docIter := q.Documents(ctx)
 
-	end := time.Now()
 	totalDocs := 0
-	for _, _ = range docIter {
-		// m := doc.Data()
+	defer docIter.Stop()
+	for {
+		doc, done := docIter.Next()
+		if done == iterator.Done {
+			break
+		}
+		if done != nil {
+			log.Fatalf("Error processing data: %v", done)
+		}
+
+		m := doc.Data()
 		totalDocs++
-		//	fmt.Printf("EventID: %v\n", m["EventID"])
+		_ = m
+		// fmt.Printf("id: %v\n", m["id"])
 	}
+	end := time.Now()
 	secs := end.Sub(start).Seconds()
-	fmt.Printf("Found %d doc in %v seconds.\n", totalDocs, secs)
+	fmt.Printf("Query completed in %v seconds.\n", secs)
+
+	log.Printf("Found %d documents", totalDocs)
 	fmt.Println("DONE")
 }
